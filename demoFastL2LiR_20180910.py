10defe4bc5ec39de4ce64a95a57c6968cf32cdff
Shuntaro Aoki <aoki@atr.jp>
2018-10-22 11:19:40 +0900

    Clean code

--- demoFastL2LiR_20180910.py
+++ demoFastL2LiR_20180910.py
@@ -1,32 +1,30 @@
+'''Demo for PyFastL2LiR'''
 
-# coding: utf-8
 
-# In[1]:
-
-#Module import
-import fastL2LiR_v2
 import numpy
 import time
+import pickle
+
+import fastl2lir
 
 
-# In[2]:
+# Preparation for simulation analysis
+X = numpy.random.rand(1200, 5000)  # The number of samples x The number of voxels
+Y = numpy.random.rand(1200, 1000)  # The number of samples x The number of CNN features
 
-#Preparation for simulation analysis
-X=numpy.random.rand(1200,5000)# The number of samples x The number of voxels
-Y=numpy.random.rand(1200,1000)# The number of samples x The number of CNN features
-alpha=1.0 #Regularization parameter (coefficient for L2-norm)
-numUsedInputFeatures=500 # The number of voxels after feature selection with correlation coefficients.
-#Prepare a model object (like scikit-learn functions)
-model = fastL2LiR_v2.fastL2LiR()
+# Regularization parameter (coefficient for L2-norm)
+alpha = 1.0
 
+# The number of voxels after feature selection with correlation coefficients.
+n_feat = 500
 
-# In[3]:
+# Prepare a model object (like scikit-learn functions)
+model = fastl2lir.fastL2LiR()
 
-#Training start
-startTime=time.time()
-model.fit(X,Y,alpha,numUsedInputFeatures)
-print 'Time for decoder training: ' + str(time.time()-startTime) + ' seconds'
+# Training
+start_t = time.time()
+model.fit(X, Y, alpha, n_feat)
+print('Time for decoder training: ' + str(time.time() - start_t) + ' seconds')
 
-#Prediction
-predicted=model.predict(X)
-
+# Prediction
+predicted = model.predict(X)

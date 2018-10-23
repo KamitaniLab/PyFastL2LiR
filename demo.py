1053de957807882a1657f99ad47c014255fa5d26
Shuntaro Aoki <aoki@atr.jp>
2018-10-23 12:48:16 +0900

    [fix] update demo.py

--- demo.py
+++ demo.py
@@ -3,14 +3,15 @@
 
 import numpy
 import time
-import pickle
 
 import fastl2lir
 
 
-# Preparation for simulation analysis
-X = numpy.random.rand(1200, 5000)  # The number of samples x The number of voxels
-Y = numpy.random.rand(1200, 1000)  # The number of samples x The number of CNN features
+# Simple example #############################################################
+
+# Preparation for simulation data
+X = numpy.random.rand(600, 1000)  # The number of samples x The number of voxels
+Y = numpy.random.rand(600, 500)   # The number of samples x The number of CNN features
 
 # Regularization parameter (coefficient for L2-norm)
 alpha = 1.0
@@ -27,4 +28,44 @@ model.fit(X, Y, alpha, n_feat)
 print('Time for decoder training: ' + str(time.time() - start_t) + ' seconds')
 
 # Prediction
-predicted = model.predict(X)
+Y_predicted = model.predict(X)
+
+
+# Multi-dimensional target features ##########################################
+
+X = numpy.random.rand(600, 1000)
+
+# The target feature array is 3D.
+Y = numpy.random.rand(600, 16, 16, 4)
+
+# Training and prediction
+model = fastl2lir.FastL2LiR()
+
+start_t = time.time()
+model.fit(X, Y, alpha, n_feat)
+print('Time for decoder training: ' + str(time.time() - start_t) + ' seconds')
+
+Y_predicted = model.predict(X)
+
+# Check predicted Y shape
+print('Predicted Y shape: %s' % (Y_predicted.shape,))
+
+
+# Chunking example ###########################################################
+
+X = numpy.random.rand(600, 1000)
+Y = numpy.random.rand(600, 16, 16, 4)
+
+chunk_size = 16
+
+# Training and prediction
+model = fastl2lir.FastL2LiR()
+
+start_t = time.time()
+model.fit(X, Y, alpha, n_feat, chunk_size=chunk_size)
+print('Time for decoder training: ' + str(time.time() - start_t) + ' seconds')
+
+Y_predicted = model.predict(X)
+
+# Check predicted Y shape
+print('Predicted Y shape: %s' % (Y_predicted.shape,))

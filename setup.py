a93d99a9397195716fdaea5b185b2317f5f6e455
Shuntaro Aoki <s_aoki@i.kyoto-u.ac.jp>
2022-01-16 17:08:34 +0900

    Version 0.8

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 
 setup(name='fastl2lir',
-      version='0.7',
+      version='0.8',
       description='Fast L2-reguralized linear regression',
       author='Kei Majima',
       author_email='brainliner-admin@atr.jp',
